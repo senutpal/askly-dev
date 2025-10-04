1055f6426e35df9eafd1fad6887d1437e2b12c56
Utpal Sen <utpalsen902@gmail.com>
2025-10-04 16:57:47 +0530

    feat(auth): add sign-in/up pages and integrate authentication across app

--- apps/web/components/providers.tsx
+++ apps/web/components/providers.tsx
@@ -1,6 +1,8 @@
 "use client";
 
-import { ConvexProvider, ConvexReactClient } from "convex/react";
+import { useAuth } from "@clerk/nextjs";
+import { ConvexReactClient } from "convex/react";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
 import * as React from "react";
 
 if (!process.env.NEXT_PUBLIC_CONVEX_URL) {
@@ -9,5 +11,9 @@ if (!process.env.NEXT_PUBLIC_CONVEX_URL) {
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL);
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <ConvexProvider client={convex}> {children}</ConvexProvider>;
+  return (
+    <ConvexProviderWithClerk useAuth={useAuth} client={convex}>
+      {children}
+    </ConvexProviderWithClerk>
+  );
 }
